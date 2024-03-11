7a4067ef853cb9b27c7ad07d5df6c0006cab14f3
shelvinbigdata <shelvinning@gmail.com>
2024-03-11 17:50:29 +0800

    set master out of code and ensure file can be accessed by all nodes in cluster.

--- 00_example/WordCount.py
+++ 00_example/WordCount.py
@@ -1,10 +1,10 @@
 # coding: utf-8
 from pyspark import SparkConf, SparkContext
 if __name__ == '__main__':
-    sparkConf = SparkConf().setMaster("local[*]").setAppName("WordCount")
+    sparkConf = SparkConf().setAppName("WordCount")
     sparkContext = SparkContext(conf=sparkConf)
 
-    file_rdd = sparkContext.textFile("../data/input/words.txt")
+    file_rdd = sparkContext.textFile("hdfs://node1:8020/warehouse/tablespace/managed/hive")
     words_rdd = file_rdd.flatMap(lambda line: line.split(" "))
     words_with_one_rdd = words_rdd.map(lambda x: (x, 1))
     count_rdd = words_with_one_rdd.reduceByKey(lambda a, b: a + b)
