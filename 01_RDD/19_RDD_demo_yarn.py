a053ea8563f9d46010367cb3dcffa68056215e9d
shelvinbigdata <shelvinning@gmail.com>
2024-03-12 18:10:18 +0800

    yarn mode need to config spark.submit.pyFiles if has dependency files.

--- 01_RDD/19_RDD_demo_yarn.py
+++ 01_RDD/19_RDD_demo_yarn.py
@@ -2,14 +2,18 @@
 from pyspark import SparkConf, SparkContext
 import json
 import os
+from city_with_category_18 import city_with_category
+
 os.environ["HADOOP_CONF_DIR"] = "/config/in/spark/env/sh"
 
 if __name__ == '__main__':
     spark_conf = SparkConf().setAppName("rdd_operator_demo").setMaster("yarn")
+    spark_conf.set("spark.submit.pyFiles", "city_with_category_18.py")
     spark_context = SparkContext(conf=spark_conf)
-    json_rdd = spark_context.textFile("hadoop://node1:8020/warehouse/test_data/order.text").flatMap(lambda line: line.split("|"))
+    json_rdd = spark_context.textFile("hadoop://node1:8020/warehouse/test_data/order.text").flatMap(
+        lambda line: line.split("|"))
     result_rdd = (json_rdd.map(lambda json_str: json.loads(json_str))
                   .filter(lambda dict_ele: dict_ele["areaName"] == "北京")
-                  .map(lambda ele: ele["areaName"] + "-" + ele["category"])
+                  .map(city_with_category)
                   .distinct())
     print(result_rdd.collect())
