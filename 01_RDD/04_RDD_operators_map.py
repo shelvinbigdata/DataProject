318d0ee4ae29474a3db224e25342553941cced2b
shelvinbigdata <shelvinning@gmail.com>
2024-03-12 14:20:37 +0800

    RDD operators flatMap, mapValues.

--- 01_RDD/04_RDD_operators_map.py
+++ 01_RDD/04_RDD_operators_map.py
@@ -13,3 +13,11 @@ if __name__ == '__main__':
 
     print(rdd.map(multiplication).collect())
     print(rdd.map(lambda num: num * 10).collect())
+
+    # flatmap
+    parallelize_rdd = spark_context.parallelize(
+        ["hello shelvin", "hadoop flink spark learn", "hello ning learn hadoop"])
+    print(parallelize_rdd.flatMap(lambda line: line.split(" ")).map(lambda word: (word, 1)).reduceByKey(
+        lambda a, b: a + b).collect())
+    # mapValues
+    print(spark_context.parallelize([("hadoop", 1), ("hadoop", 2), ("flink", 3)]).mapValues(lambda v: v * 10).collect())
