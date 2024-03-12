b8a2f599597274684ebd2de6ca05f024df1f6d5d
shelvinbigdata <shelvinning@gmail.com>
2024-03-12 10:43:52 +0800

    parallelize create RDD.

---
+++ 01_RDD/01_RDD_create_parallelize.py
@@ -0,0 +1,13 @@
+# coding: utf8
+from pyspark import SparkConf, SparkContext
+
+if __name__ == '__main__':
+    spark_conf = SparkConf().setAppName("parallelize_rdd")
+    spark_context = SparkContext(conf=spark_conf)
+    parallelize_rdd = spark_context.parallelize([1, 2, 3, 4, 5, 6, 7, 8, 9])
+    print("default partitions: ", parallelize_rdd.getNumPartitions())
+
+    parallelize_rdd = spark_context.parallelize([1,2,3], 3)
+    print("config partitions: ", parallelize_rdd.getNumPartitions())
+
+    print("rdd content: ", parallelize_rdd.collect())
