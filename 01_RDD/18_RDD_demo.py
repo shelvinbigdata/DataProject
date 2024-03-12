b9b3a38a3694f582412357f1ab3e07e380e8ab54
shelvinbigdata <shelvinning@gmail.com>
2024-03-12 18:06:21 +0800

    extract method.

--- 01_RDD/18_RDD_demo.py
+++ 01_RDD/18_RDD_demo.py
@@ -1,6 +1,7 @@
 # coding:utf8
 from pyspark import SparkConf, SparkContext
 import json
+from city_with_category_18 import city_with_category
 
 if __name__ == '__main__':
     spark_conf = SparkConf().setAppName("rdd_operator_demo")
@@ -8,6 +9,6 @@ if __name__ == '__main__':
     json_rdd = spark_context.textFile("../test_data/order.text").flatMap(lambda line: line.split("|"))
     result_rdd = (json_rdd.map(lambda json_str: json.loads(json_str))
                   .filter(lambda dict_ele: dict_ele["areaName"] == "北京")
-                  .map(lambda ele: ele["areaName"] + "-" + ele["category"])
+                  .map(city_with_category)
                   .distinct())
     print(result_rdd.collect())
---
+++ 01_RDD/city_with_category_18.py
@@ -0,0 +1,4 @@
+# coding: utf8
+
+def city_with_category(data):
+    return data["areaName"] + "-" + data["category"]
