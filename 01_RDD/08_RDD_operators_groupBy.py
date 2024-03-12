296d10d047ec604ad310e76ffb1f463ca5c61e70
shelvinbigdata <shelvinning@gmail.com>
2024-03-12 15:06:18 +0800

    RDD operators distinct.

--- 01_RDD/08_RDD_operators_groupBy.py
+++ 01_RDD/08_RDD_operators_groupBy.py
@@ -5,6 +5,6 @@ from pyspark import SparkConf, SparkContext
 if __name__ == '__main__':
     spark_conf = SparkConf().setAppName("operator_groupBy")
     spark_context = SparkContext(conf=spark_conf)
-    print(spark_context.parallelize([("a", 1), ("b", 2), ("c", 1), ("b", 4), ("a", 7)]).filter(
-        lambda t: t[1] % 2 == 1).groupBy(lambda t: t[0]).mapValues(
+    print(spark_context.parallelize([("a", 1), ("b", 2), ("c", 1), ("b", 4), ("a", 7), ("a", 1), ("c", 1)]).filter(
+        lambda t: t[1] % 2 == 1).distinct().groupBy(lambda t: t[0]).mapValues(
         lambda v: list(v)).collect())
