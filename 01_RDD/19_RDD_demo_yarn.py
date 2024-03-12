68f643066c31a561b5eedcee3d0a43ffaeb824db
shelvinbigdata <shelvinning@gmail.com>
2024-03-12 18:04:07 +0800

    RDD operators demo run on yarn.

---
+++ 01_RDD/19_RDD_demo_yarn.py
@@ -0,0 +1,15 @@
+# coding:utf8
+from pyspark import SparkConf, SparkContext
+import json
+import os
+os.environ["HADOOP_CONF_DIR"] = "/config/in/spark/env/sh"
+
+if __name__ == '__main__':
+    spark_conf = SparkConf().setAppName("rdd_operator_demo").setMaster("yarn")
+    spark_context = SparkContext(conf=spark_conf)
+    json_rdd = spark_context.textFile("hadoop://node1:8020/warehouse/test_data/order.text").flatMap(lambda line: line.split("|"))
+    result_rdd = (json_rdd.map(lambda json_str: json.loads(json_str))
+                  .filter(lambda dict_ele: dict_ele["areaName"] == "北京")
+                  .map(lambda ele: ele["areaName"] + "-" + ele["category"])
+                  .distinct())
+    print(result_rdd.collect())
